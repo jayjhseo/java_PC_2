83d206f18e0d60f14059bbf1b04fec698ad881d5
jayjhseo <jayjhseo@gmail.com>
2023-09-18 17:22:38 +0900

    test_01

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,7 +11,7 @@ public class Main {
         // Press Alt+Shift+X or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
-            // Press Alt+Shift+D to start debugging your code. We have set one breakpoint
+
             // for you, but you can always add more by pressing Ctrl+Shift+B.
             System.out.println("i = " + i);
         }
